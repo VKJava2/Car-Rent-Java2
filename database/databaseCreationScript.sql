7d25505815984f4a09d96677d7759d01ce3c46b6
VKJava2 <VK>
2015-07-16 20:34:12 +0300

    database script update

--- database/databaseCreationScript.sql
+++ database/databaseCreationScript.sql
@@ -2,15 +2,15 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `Java2_test` DEFAULT CHARACTER SET utf8 ;
-USE `Java2_test` ;
+CREATE SCHEMA IF NOT EXISTS `car_rent` DEFAULT CHARACTER SET utf8 ;
+USE `car_rent` ;
 
 -- -----------------------------------------------------
 -- Table `Java2_test`.`users`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `java2_test`.`users` ;
+DROP TABLE IF EXISTS `car_rent`.`users` ;
 
-CREATE TABLE IF NOT EXISTS `java2_test`.`users` (
+CREATE TABLE IF NOT EXISTS `car_rent`.`users` (
   `UserID` INT(11) NOT NULL AUTO_INCREMENT,
   `FirstName` CHAR(32) NOT NULL,
   `LastName` CHAR(32) NOT NULL,
