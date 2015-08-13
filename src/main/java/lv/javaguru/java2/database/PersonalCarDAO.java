69e0418a19609e84695d232887036e166351c8c3
KanoJG <partner1@inbox.lv>
2015-08-13 15:37:03 +0300

    PersonalCar DAO and DAOImpl ready

---
+++ src/main/java/lv/javaguru/java2/database/PersonalCarDAO.java
@@ -0,0 +1,28 @@
+package lv.javaguru.java2.database;
+
+
+import lv.javaguru.java2.domain.PersonalCar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface PersonalCarDAO {
+
+    void create(PersonalCar personalCar) throws DBException;
+
+    PersonalCar getById(String id) throws DBException; // 1 car
+
+    List<PersonalCar> getByMake(String manufacturer) throws DBException; // one or many = List
+
+    List<PersonalCar> getByLuxuryType(String luxuryType) throws DBException; // one or many = List
+
+    List<PersonalCar> getByBodyType(String bodyType) throws DBException; // one or many = List
+
+    List<PersonalCar> getByFuelType(String fuelType) throws DBException; // one or many = List
+
+    void delete(Long id) throws DBException;
+
+    void update(PersonalCar personalCar, Long carId) throws DBException;
+
+    List<PersonalCar> getAll(String vehicleType) throws DBException;
+}
